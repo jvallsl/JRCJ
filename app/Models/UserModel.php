ba9c9a6130de9f27b25111558d7e4d16f2b92ac5
RaulBgm <74044182+RaulBgm@users.noreply.github.com>
2020-12-21 15:25:46 +0100

    Cambios en la definicion de la sesion

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -8,7 +8,7 @@
         protected $table = 'usuario';
         protected $primaryKey = "UsuarioId";
         protected $allowedFields = ['Username', 'Contrasena', 'Email',
-        'Experiencia', 'Activo', 'Administrador'];
+        'Registro','Experiencia', 'Activo', 'Administrador'];
 
         public function getUsers($userName = false){
 
@@ -33,10 +33,12 @@
             }
 
             if(isset($datos_usuario)){
-
+                $vmodel = new ValoracionModel();
                 #Damos formato dd/mm/YYYY a la fecha de registro.
                 $fecha_registro = strtotime($datos_usuario['Registro']);
                 $fecha_registro = strftime('%d/%m/%Y', $fecha_registro);
+
+                $valoraciones = $vmodel->getValoraciones($datos_usuario['UsuarioId']);
 
                 $usuario=[
                     'UsuarioId' => $datos_usuario['UsuarioId'],
@@ -44,16 +46,13 @@
                     'Email'=> $datos_usuario['Email'],
                     'Registro' => $fecha_registro,
                     'Experiencia'=> $datos_usuario['Experiencia'],
-                    'Admin'=>$datos_usuario['Administrador']
+                    'Admin'=>$datos_usuario['Administrador'],
+                    'Valoraciones' =>$valoraciones
                 ];
                 $session = session();
                 $session->set($usuario);
 
-                $vmodel = new ValoracionModel();
-                $valoraciones = $vmodel->getValoraciones($datos_usuario['UsuarioId']);
-                $session->set('valoraciones' , $valoraciones);
-
-                return $datos_usuario;
+                return $usuario;
             }else{
 
                 $datos_usuario = null;
@@ -95,6 +94,15 @@
                 return;
             }
         }
+
+        public function actualizarExperienciaModel($experiencia, $usuarioId){
+            $nueva_experiencia = ['Experiencia' => $experiencia];
+            $this->builder()->where('UsuarioId', $usuarioId);
+            $this->builder()->update($nueva_experiencia);
+            session()->set('Experiencia', $experiencia);
+            return;
+        }
+
 
 
         // public function get($UsuarioId = null)
