4baa924967a21ca5fcb104e7926bb66139692bfe
RaulBgm <74044182+RaulBgm@users.noreply.github.com>
2021-01-04 13:54:36 +0100

    Añadido administracion productos.

--- app/Views/pages/admin_products.php
+++ app/Views/pages/admin_products.php
@@ -1,21 +1,20 @@
 
-<body>
- 		
-		<table>
-			<tr><td>Numeración</td><td>Nombre</td><td>Descripción</td><td>Imagen</td></tr>
-			<?php
-			foreach ($productos as $producto):
-				?>				
-					<tr class='producto'>
-						<th><?php echo $producto->ProductoId; ?></th>
-	    				<th><?php echo $producto->Nombre; ?></th>
-	    				<th><?php echo $producto->Descripcion; ?></th>
-	    				<th><img name='<?php echo $producto->Imagen; ?>' src='/img/<?php echo $producto->Imagen; ?>' width='100px' height='100px'/></th>
-	    				<th><a href=<?=base_url('admin/update/'.$producto->ProductoId)?> style="padding:20px 10px 20px 10px; background-color:green; color:white; text-decoration:none;">Update</a></th>
-	    				<th><a href="<?php echo base_url('admin/erase/'.$producto->ProductoId);?>" style="padding:20px 10px 20px 10px; background-color:red; color:white;text-decoration:none;">Erase</a></th>
-				    </tr>
-				<?php endforeach; ?>
-		</table>
+<body>	
+	<table>
+		<tr><td>Numeración</td><td>Nombre</td><td>Descripción</td><td>Imagen</td></tr>
+		<?php
+		foreach ($productos as $producto):
+			?>				
+				<tr class='producto'>
+					<th><?php echo $producto->ProductoId; ?></th>
+					<th><?php echo $producto->Nombre; ?></th>
+					<th><?php echo $producto->Descripcion; ?></th>
+					<th><img name='<?php echo $producto->Imagen; ?>' src='/img/<?php echo $producto->Imagen; ?>' width='100px' height='100px'/></th>
+					<th><a href=<?=base_url('admin/update/='.$producto->ProductoId)?> style="padding:20px 10px 20px 10px; background-color:green; color:white; text-decoration:none;">Update</a></th>
+					<th><a href="<?php echo base_url('admin/erase/='.$producto->ProductoId);?>" style="padding:20px 10px 20px 10px; background-color:red; color:white;text-decoration:none;">Erase</a></th>
+				</tr>
+			<?php endforeach; ?>
+	</table>
 
 </body>
 </html>
